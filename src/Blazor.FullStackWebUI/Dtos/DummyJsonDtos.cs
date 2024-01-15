dcb6c3984d4548d5933ef9f568fff7f0c4bca203
Diego Zunino <diego.zunino@ellycode.com>
2024-01-15 07:02:12 +0100

    Add SSR examples

---
+++ src/Blazor.FullStackWebUI/Dtos/DummyJsonDtos.cs
@@ -0,0 +1,13 @@
+namespace Blazor.FullStackWebUI.Dtos;
+
+public class DummyJsonListDto
+{
+    public int Total { get; set; }
+    public int Skip { get; set; }
+    public int Limit { get; set; }
+}
+
+public class PostListDto : DummyJsonListDto
+{
+    public List<Post> Posts { get; set; }
+}
