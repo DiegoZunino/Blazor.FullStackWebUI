7212553f898b45f816b9b2a5b5bdc67bfdee2d9c
Diego Zunino <diego.zunino@ellycode.com>
2024-01-18 22:05:03 +0100

    Demo start

--- src/Blazor.FullStackWebUI/Program.cs
+++ src/Blazor.FullStackWebUI/Program.cs
@@ -1,27 +1,20 @@
-using System.Net.Sockets;
-
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddRazorComponents();
 
 builder.Services.AddHttpClient(DummyJsonService.ClientName,
     client => client.BaseAddress = new Uri(DummyJsonService.BaseAddress));
-
 builder.Services.AddSingleton<IDummyJsonService<Post>, PostService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-
 app.UseStaticFiles();
 app.UseAntiforgery();
 
